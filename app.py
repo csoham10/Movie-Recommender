268617c7f35438400715b9fe1202fdc4f57cff61
csoham <sohampc10@gmail.com>
2024-05-15 23:07:30 +0530

    added pickle file download

--- app.py
+++ app.py
@@ -24,6 +24,8 @@ def recommend(movie):
     return recommended_movie_names, recommended_movie_posters
 
 
+#download movies_dict.pkl and similarity.pkl from google drive: https://drive.google.com/drive/folders/1mA3hC6ifX8z-MySU64uMt_Af-x_Du_YU?usp=sharing
+
 st.header('Movie Recommendation System')
 movies_dict = pickle.load(open('movie_dict.pkl', 'rb'))
 movies = pd.DataFrame(movies_dict)
